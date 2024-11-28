e9d6fbeefe53942a69c6fb05add0629e0984c7e0
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-28 09:17:42 +0100

    Commit

---
+++ Provera validnosti username-a/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Unesite username: ");
+        string username = Console.ReadLine();
+
+        if (ValidirajUsername(username))
+        {
+            Console.WriteLine("Username je validan.");
+        }
+        else
+        {
+            Console.WriteLine("Username nije validan. Mora imati između 5 i 15 znakova i ne sme sadržavati '@'.");
+        }
+    }
+
+    static bool ValidirajUsername(string username)
+    {
+        if (username.Length < 5 || username.Length > 15)
+            return false;
+
+        if (username.Contains("@"))
+            return false;
+
+        return true;
+    }
+}
+
